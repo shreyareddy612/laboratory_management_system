d120fd8b5271d9f6b75d600e838bd98a2b2f01ef
Mbicha <mbithicharlse@gmail.com>
2023-08-11 16:59:00 +0300

    Created Display Profile Component

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import profile from "../images/person.png";
 import account from "../images/test-account.png"
+import DisplayProfile from "../components/DisplayProfile";
 
 import http from "../http-common";
 
@@ -320,68 +321,7 @@ const Profile = ({ user = JSON.parse(localStorage.getItem("user")) }) => {
                                     </div>
                                 </form>  
                             ) : (
-                                <div className="border rounded-lg p-2">
-                                    <div className="flex flex-col p-1 m-1">                            
-                                        <span className="flex border-b-2 font-bold">
-                                            Description:
-                                        </span>{profileData.description}
-                                    </div>
-                                    <section className="flex flex-col p-2 sm:flex-row">
-                                        {/* Personal Details */}
-                                        <div className="w-full border rounded-md p-1 m-1 sm:w-1/2">
-                                            <h2 className="h2 border-b-2 mb-1"> Personal Information</h2>
-                                            <div className="label-row">                            
-                                                <span className="flex font-bold">
-                                                    Sex:
-                                                </span>{profileData.sex}
-                                            </div>
-                                            <div className="label-row">                            
-                                                <span className="flex font-bold">
-                                                    Marital Status:
-                                                </span>{profileData.marital_status}
-                                            </div>
-                                            <div className="label-row">                            
-                                                <span className="flex font-bold">
-                                                    Number of Children:
-                                                </span>{profileData.children}
-                                            </div>
-                                            <div className="label-row">                            
-                                                <span className="flex font-bold">
-                                                    Blood Group:
-                                                </span>{profileData.blood_group}
-                                            </div>
-                                            <div className="label-row">                            
-                                                <span className="flex font-bold">
-                                                    Work At: 
-                                                </span>{profileData.work_at}
-                                            </div>
-                                            <div className="label-row">                            
-                                                <span className="flex font-bold">
-                                                    Profession:
-                                                </span>{profileData.work}
-                                            </div>
-                                        </div>
-                                        {/* Next of Kin */}
-                                        <div className="w-full border rounded-md p-1 m-1 sm:w-1/2">
-                                            <h2 className="h2 border-b-2 mb-1"> Next of Kin</h2>
-                                            <div className="label-row">                            
-                                                <span className="flex font-bold">
-                                                    Name:
-                                                </span>{profileData.next_of_kin}
-                                            </div>
-                                            <div className="label-row">                            
-                                                <span className="flex font-bold">
-                                                    Relationship:
-                                                </span>{profileData.next_of_kin_rel}
-                                            </div>
-                                            <div className="label-row">                            
-                                                <span className="flex font-bold">
-                                                    Contact:
-                                                </span>{profileData.next_of_kin_contact}
-                                            </div>
-                                        </div>
-                                    </section>          
-                                </div>
+                                <DisplayProfile profileData={profileData} />
                             )
                         )                        
                     }
